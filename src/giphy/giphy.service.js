fb58992fb8b2e81787c1872ce4e26fe329d3fd2d
Dima <stealss7@gmail.com>
2017-03-09 22:28:11 +0200

    https protocol for giphy

--- src/giphy/giphy.service.js
+++ src/giphy/giphy.service.js
@@ -7,6 +7,6 @@ angular.
 module('giphy').
 factory('Giphy', ['$resource',
     function($resource) {
-        return $resource('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=:tag');
+        return $resource('https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=:tag');
     }
 ]);
